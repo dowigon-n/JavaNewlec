c8e68b79a874ec0eb422b01dcc0ad58ed2e5d6c9
ginnyx103 <ginnyx103@daum.net>
2022-04-18 00:46:01 +0900

    first commit

---
+++ Newlec/src/javacode_basic_rule.java
@@ -0,0 +1,25 @@
+
+public class javacode_basic_rule {
+
+	// 변수 선언 : 이 기호들을 변수라고 선언하는 코드
+	// 자료형을 앞에 먼저 선언해야 한다.
+	int kor1, kor2, kor3;
+	int total;
+	float avg;
+	
+	// 컴파일러가 알 수 없는 기호들
+	// kor1 = 50;
+	// kor2 = 60;
+	// kor3 = 80;
+	
+	// total = kor1 + kor2 + kor3
+	// avg = total;
+	
+	public static void main(String[] args) {
+		
+		// 콘솔 출력 API 함수
+		System.out.println();
+
+	}
+
+}
